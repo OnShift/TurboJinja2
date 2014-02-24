973be6e561c8dead29707774e4842343bd824617
Stephen J. Fuhry <sfuhry@onshift.com>
2014-02-24 10:22:18 -0500

    template tags

--- turbojinja/widgets.py
+++ turbojinja/widgets.py
@@ -2,11 +2,23 @@
 from __future__ import absolute_import, division, print_function
 
 import os
+import inspect
 import jinja2
+from bazman import templatetags
+
+
+def is_mod_function(mod, func):
+    return inspect.isfunction(func) and inspect.getmodule(func) == mod
+
+
+def list_functions(mod):
+    return [func for func in mod.__dict__.itervalues()
+            if is_mod_function(mod, func)]
 
 
 class TurboJinja(object):
     extension = "html"
+    templatetags = None
 
     def __init__(self, extra_vars_func=None, options=None):
         self.get_extra_vars = extra_vars_func
@@ -14,6 +26,11 @@ class TurboJinja(object):
             self.options = options
         else:
             self.options = dict()
+
+        self.__auto_register_module(templatetags)
+
+    def __auto_register_module(self, module):
+        self.templatetags = list_functions(module)
 
     def load_template(self, template_name):
         """template_name == dotted.path.to.template (without .ext)
@@ -32,6 +49,9 @@ class TurboJinja(object):
         templates_path = package.replace(".", os.sep)
         template_loader = jinja2.FileSystemLoader(searchpath=templates_path)
         template_env = jinja2.Environment()
+        if self.templatetags:
+            for filt in self.templatetags:
+                template_env.filters[filt.__name__] = filt
         template_env.loader = template_loader
         template_obj = template_env.get_template(
             '%s.%s' % (basename, self.extension)
