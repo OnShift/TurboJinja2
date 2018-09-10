9cc882ded195ec1e64337d9b93b9c697b61f1eb2
Nate Kelling <nkelling@onshift.com>
2018-09-10 10:27:31 -0400

    more testing

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     #download_url=download_url,
     license=license,
 
-    install_requires=["Jinja2 >= 2.7"],
+    install_requires=["TurboGears == 1.0.11.10","Jinja2 >= 2.7"],
     zip_safe=False,
     packages=find_packages(),
     package_data=find_package_data(where='turbojinja',
